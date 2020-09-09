6270131e7975f6311d825ff6efca33635ee8b32f
Slogreco <shoshwright@hotmail.com>
2020-09-09 13:51:23 -0700

    added additional buttons on HTML. created an output variable inside of my start quiz function

--- script.js
+++ script.js
@@ -45,7 +45,27 @@ let quizQuestions = [
 ]
 
 function startQuiz() {
+    let output = [];
 
+    quizQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            let answers = [];
+            for (letter in currentQuestion.answers) {
+                answers.push(
+                    `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${letter} :
+                    ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+    quizContainer.innerHTML = output.join('');
 }
 
 function showResults() {
