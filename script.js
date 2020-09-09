3a89e7b2514daf39bec2ab08b65762ccec1f1690
Slogreco <shoshwright@hotmail.com>
2020-09-09 13:27:58 -0700

    added functions to fill in and an event listener for my button

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ let quizContainer = document.getElementById("quiz");
 let resultsContainer = document.getElementById("results");
 let submitButton = document.getElementById("submit");
 
+function startQuiz() {
+
+}
+
+function showResults() {
+
+}
+
+submitButton.addEventListener("click", showResults);
