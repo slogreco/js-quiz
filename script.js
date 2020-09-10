e31b0f4bca512255233464a5bcd38e37964fb0ce
Slogreco <shoshwright@hotmail.com>
2020-09-09 19:56:09 -0700

    sourced my javascript to my HTML so it will actually work. start quiz function coded

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let quizContainer = document.getElementById("quiz");
-let resultsContainer = document.getElementById("results");
-let submitButton = document.getElementById("submit");
 
-let quizQuestions = [
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+const quizQuestions = [
     {
         question: "Inside which HTML element do we put JavaSCript?",
         answers: {
@@ -44,27 +44,46 @@ let quizQuestions = [
     },
 ]
 
-function startQuiz() {
-    let output = [];
 
+
+
+
+console.log(quizQuestions);
+
+function startQuiz() {
+
+    // variable to store the HTML output
+    const output = [];
+
+    // for each question...
     quizQuestions.forEach(
         (currentQuestion, questionNumber) => {
-            let answers = [];
+
+            // variable to store the list of possible answers
+            const answers = [];
+
+            // and for each available answer...
             for (letter in currentQuestion.answers) {
+
+                // ...add an HTML radio button
                 answers.push(
                     `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                    </label>`
+                  <input type="radio" name="question${questionNumber}" value="${letter}">
+                  ${letter} :
+                  ${currentQuestion.answers[letter]}
+                </label>`
                 );
             }
+
+            // add this question and its answers to the output
             output.push(
                 `<div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>`
+              <div class="answers"> ${answers.join('')} </div>`
             );
         }
     );
+
+    // finally combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
 }
 
