ec711cc9a5a1782fc3f3a07311103c3874be0e74
Slogreco <shoshwright@hotmail.com>
2020-09-10 06:47:10 -0700

    got my questions to show up

--- script.js
+++ script.js
@@ -1,53 +1,6 @@
-let quizContainer = document.getElementById("quiz");
-let resultsContainer = document.getElementById("results");
-let submitButton = document.getElementById("submit");
-let quizQuestions = [
-    {
-        question: "Inside which HTML element do we put JavaSCript?",
-        answers: {
-            a: "<js>",
-            b: "<scripting>",
-            c: "<javascript>",
-            d: "<script>"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "How do you create a function in JavaScript?",
-        answers: {
-            a: "function myFunction()",
-            b: "function = myFunction()",
-            c: "function:myFunction()",
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "How does a FOR loop start?",
-        answers: {
-            a: "for (i = 0; i <= 5)",
-            b: "for (i = 0; i <= 5; i++)",
-            c: "for i = 1 to 5",
-            d: "for (i <= 5; i++)"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Which operator is used to assign a value to a variable?",
-        answers: {
-            a: "X",
-            b: "*",
-            c: "=",
-            d: "-"
-        },
-        correctAnswer: "c"
-    },
-]
 
 
 
-
-
-console.log(quizQuestions);
 
 function startQuiz() {
 
@@ -86,7 +39,7 @@ function startQuiz() {
     quizContainer.innerHTML = output.join('');
 }
 
-startQuiz();
+
 
 function showResults() {
     let answerContainers = quizContainer.querySelectorAll(".answers");
@@ -112,6 +65,51 @@ function showResults() {
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
+let quizContainer = document.getElementById("quiz");
+let resultsContainer = document.getElementById("results");
+let submitButton = document.getElementById("submit");
+let quizQuestions = [
+    {
+        question: "Inside which HTML element do we put JavaSCript?",
+        answers: {
+            a: "js",
+            b: "scripting",
+            c: "javascript",
+            d: "script"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: {
+            a: "function myFunction()",
+            b: "function = myFunction()",
+            c: "function:myFunction()",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: {
+            a: "for (i = 0; i <= 5)",
+            b: "for (i = 0; i <= 5; i++)",
+            c: "for i = 1 to 5",
+            d: "for (i <= 5; i++)"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answers: {
+            a: "X",
+            b: "*",
+            c: "=",
+            d: "-"
+        },
+        correctAnswer: "c"
+    },
+]
 
+startQuiz();
 
 submitButton.addEventListener("click", showResults);
